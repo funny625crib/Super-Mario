16435c99396a88da5536e8be205368dc0243d69b
504-19\cre <a>
2025-06-07 18:48:24 +0900

    キリコ処理のソースコード

---
+++ program/agaric.h
@@ -0,0 +1,17 @@
+#pragma once
+#pragma once
+#include "Float2.h"
+#include "base.h"
+
+class Agaric :
+	public Base {
+
+
+	void Init();     //	‰Šú‰»ˆ—
+	void Update();	//	XVˆ—
+	void Render();	//	•`‰æˆ—
+	void Exit();		//	I—¹ˆ—
+
+
+
+};
